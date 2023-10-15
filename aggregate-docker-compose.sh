3616c34e329079d9894c965e2194d4763f948196
Ben Pearce <me@benpearce.io>
2023-10-15 15:56:08 +0100

    feat: :sparkles: switch to new docker compose `include` syntax

--- aggregate-docker-compose.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-BASEDIR=$(dirname "$0")
-export $(awk 1 $BASEDIR/.env/*.env | xargs) && /usr/bin/docker compose $(find {$BASEDIR/docker*.yml,$BASEDIR/dependencies/docker*.yml} | sed -e 's/^/-f /') "$@"
